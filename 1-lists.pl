a46737eecd81e9cf369587575b91d7119b4a2206
Johannes Skivdal <johannes@skivdaldata.no>
2024-10-28 00:47:02 +0100

    Problems 2 and 3

--- 1-lists.pl
+++ 1-lists.pl
@@ -2,3 +2,11 @@
 my_last(X, [X]).
 my_last(X, [_ | Rest]) :- my_last(X, Rest).
 
+/* Problem 2: find the last but one element of a list */
+penultimate(X, [X | [_ | []]]).
+penultimate(X, [_ | Rest]) :- penultimate(X, Rest).
+
+/* Problem 3: Find the K'th element of a list */
+element_at(X, [X | _], 1).
+element_at(X, [_ | Rest], K) :- Ka is K - 1, element_at(X, Rest, Ka).
+
