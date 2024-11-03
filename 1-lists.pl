e33a2a12fa70fd2bb37675fca67d0b081ee7a644
Johannes Skivdal <johannes@skivdaldata.no>
2024-11-03 02:17:14 +0100

    Better pattern matchings

--- 1-lists.pl
+++ 1-lists.pl
@@ -11,24 +11,24 @@ element_at(X, [X | _], 1).
 element_at(X, [_ | Rest], K) :- Ka is K - 1, element_at(X, Rest, Ka).
 
 /* Problem 4: Find the number of elements in a list */
-list_len(X, []) :- X is 0.
+list_len(0, []).
 list_len(X, [_ | Rest]) :- list_len(Y, Rest), X is Y + 1.
 
 /* Problem 5: Reverse a list */
-list_rev(X, []) :- X = [].
+list_rev([], []).
 list_rev(X, [A | Rest]) :- list_rev(Y, Rest), append(Y, [A], X).
 
 /* Problem 6: Find out whether a list is a palindrome */
 is_palindrome(X) :- list_rev(Y, X), X == Y.
 
 /* Problem 7: Flatten a nested list structure */
-my_flatten([], X) :- X = [].
+my_flatten([], []).
 my_flatten([A | Rest], X) :- is_list(A), !, my_flatten(A, Ap), my_flatten(Rest, Y), append(Ap, Y, X).
 my_flatten([A | Rest], X) :- my_flatten(Rest, Y), append([A], Y, X).
 
 /* Problem 8: Eliminate consecutive duplicates of list elements */
-compress([], X) :- X = [].
-compress([A], X) :- X = [A].
+compress([], []).
+compress([A], [A]).
 compress([A | Rest], X) :- compress(Rest, Y), [F | _] = Y, A \= F, append([A], Y, X).
 compress([_ | Rest], X) :- compress(Rest, X).
 
